b3e8efaf172d44c23a51f77f471d4ede7767a77d
guotianliang <1121663349@qq.com>
2019-02-22 15:56:17 +0800

    修改

---
+++ data.js
@@ -0,0 +1,4 @@
+var jsonStr=[];
+
+module.exports.jsonStr=jsonStr;
+jsonStr[123456]=12356;
--- yan.js
+++ yan.js
@@ -1,5 +1,7 @@
 var http=require("http")
 var url=require("url")
+var fs=require("fs")
+var data=require("./data")
 
 http.createServer((req,res)=>{
     res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'})
@@ -7,7 +9,14 @@ http.createServer((req,res)=>{
     if(req.url==="/favicon.ico"){
         return
     }
-    if(obj.username=="123456"&&obj.password=="1111"&&obj.test=="0000"){
+    // fs.appendFile("./data.js",`jsonStr[${obj.username}]=${obj.password};`,(err)=>{
+    //     if(err){
+    //         console.log(err)
+    //     }else{
+    //         res.end("注册成功")
+    //     }
+    // })
+    if(obj.password==data.jsonStr[obj.username]&&obj.test=="0000"){
         res.end("登录成功,欢迎您回来"+obj.username)
     }else{
         res.end("登录失败 密码或验证码错误")
